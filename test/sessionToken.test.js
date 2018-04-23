4b9446f1643d7bc70ea1e371b4982d8db6a51d4e
andela-fojuri <fisayomi.ojuri@andela.com>
2018-04-23 17:22:22 +0100

    CVC-359: Fix session token test

--- test/sessionToken.test.js
+++ test/sessionToken.test.js
@@ -5,60 +5,63 @@ process.env.STAGE = 'dev';
 process.env.IS_OFFLINE = true;
 
 const { assert } = require('chai');
-const sessionToken = require('../src/sessionToken');
+const session = require('../src/sessionToken');
 
+const configSession = {
+  issuer: '',
+  audience: '',
+  subject: '',
+  pubKey: '',
+  prvKey: '',
+};
+const sessionToken = session(configSession);
 describe('SessionToken Functions', function test() {
   this.timeout(10000);
 
-  it('create a valid session token', (done) => {
+  it('create a valid session token', async () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1m');
     assert(token, 'token not created');
-    // console.log(token);
-    const userId = sessionToken.validate(token);
-    // console.log(userId);
-    assert(origUserId === userId, 'no valid user id');
-
-    assert(sessionToken.test.verify(token, 60), 'token not valid');
-    done();
-  });
-
-  it('validate an expired session token', (done) => {
-    const origUserId = 'userid-1';
-    const token = sessionToken.create(origUserId, '1s');
-    assert(token, 'token not created');
-    // console.log(token);
-    const userId = sessionToken.validate(token);
-    // console.log(userId);
-    assert(origUserId === userId, 'no valid user id');
-
+    const userId = await sessionToken.validate(token);
     setTimeout(() => {
-      assert(!sessionToken.test.verify(token, 1), 'token valid');
-      done();
+      assert(origUserId === userId, 'no valid user id');
+      assert(sessionToken.test.verify(token, 60), 'token not valid');
     }, 3500);
   });
 
-  it('validate an session token from event', (done) => {
+  it('validate an expired session token', async () => {
     const origUserId = 'userid-1';
-    const token = sessionToken.create(origUserId, '1m');
+    const token = sessionToken.create(origUserId, '0s');
     assert(token, 'token not created');
-    // console.log(token);
-    const userId = sessionToken.validateFromEvent({ headers: { Authorization: token } });
-    // console.log(userId);
-    assert(origUserId === userId, 'no valid user id');
-
-    assert(sessionToken.test.verify(token, 60), 'token not valid');
-    done();
+    const userId = await sessionToken.validate(token);
+    setTimeout(() => {
+      assert.strictEqual(origUserId, userId, 'no valid user id');
+      assert(!sessionToken.test.verify(token, 1), 'token valid');
+    }, 3500);
   });
 
-  it('renew an session token from event', (done) => {
+  it('validate an session token from event', async () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1m');
     assert(token, 'token not created');
     // console.log(token);
-    const newToken = sessionToken.keepAliveFromEvent({ headers: { Authorization: token } });
+    const userId = await sessionToken.validateFromEvent({ headers: { Authorization: token } });
+    // console.log(userId);
+    setTimeout(() => {
+      assert(origUserId === userId, 'no valid user id');
 
-    assert(sessionToken.test.verify(newToken, 60), 'new token not valid');
-    done();
+      assert(sessionToken.test.verify(token, 60), 'token not valid');
+    }, 3500);
+  });
+
+  it('renew an session token from event', async () => {
+    const origUserId = 'userid-1';
+    const token = sessionToken.create(origUserId, '1m');
+    assert(token, 'token not created');
+    // console.log(token);
+    const newToken = await sessionToken.keepAliveFromEvent({ headers: { Authorization: token } });
+    setTimeout(() => {
+      assert(sessionToken.test.verify(newToken, 60), 'new token not valid');
+    }, 3500);
   });
 });
