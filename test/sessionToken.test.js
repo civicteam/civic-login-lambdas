a6d39b153d96dbc06e8d2257299bf7e55a30b1b4
andela-fojuri <fisayomi.ojuri@andela.com>
2018-04-26 19:17:15 +0100

    CVC-251: fix tests

--- test/sessionToken.test.js
+++ test/sessionToken.test.js
@@ -2,15 +2,9 @@
 
 const { expect } = require('chai');
 const session = require('../src/sessionToken');
+const { config } = require('../assets/tests');
 
-const configSession = {
-  issuer: '',
-  audience: '',
-  subject: '',
-  pubKey: '',
-  prvKey: '',
-};
-const sessionToken = session(configSession);
+const sessionToken = session(config.sessionToken);
 describe('SessionToken Functions', function test() {
   this.timeout(10000);
 
@@ -20,10 +14,8 @@ describe('SessionToken Functions', function test() {
     expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = sessionToken.validate(token);
-    setTimeout(() => {
-      expect(origUserId).to.equal(userId);
-      expect(sessionToken.test.verify(token, 60)).to.be.true;
-    }, 3500);
+    expect(origUserId).to.equal(userId);
+    expect(sessionToken.test.verify(token, 60)).to.be.true;
   });
 
   it('validate an expired session token', async () => {
@@ -32,10 +24,8 @@ describe('SessionToken Functions', function test() {
     expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = await sessionToken.validate(token);
-    setTimeout(() => {
-      expect(origUserId).to.equal(userId);
-      expect(sessionToken.test.verify(token, 1)).to.be.true;
-    }, 3500);
+    expect(origUserId).to.equal(userId);
+    expect(sessionToken.test.verify(token, 1)).to.be.true;
   });
 
   it('validate an session token from event', async () => {
@@ -44,10 +34,8 @@ describe('SessionToken Functions', function test() {
     expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = await sessionToken.validateFromEvent({ headers: { Authorization: token } });
-    setTimeout(() => {
-      expect(origUserId).to.equal(userId);
-      expect(sessionToken.test.verify(token, 60)).to.be.true;
-    }, 3500);
+    expect(origUserId).to.equal(userId);
+    expect(sessionToken.test.verify(token, 60)).to.be.true;
   });
 
   it('renew an session token from event', async () => {
@@ -56,8 +44,6 @@ describe('SessionToken Functions', function test() {
     expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const newToken = await sessionToken.keepAliveFromEvent({ headers: { Authorization: token } });
-    setTimeout(() => {
-      expect(sessionToken.test.verify(newToken, 60)).to.be.true;
-    }, 3500);
+    expect(sessionToken.test.verify(newToken, 60)).to.be.true;
   });
 });
