3630c427fceefba2e9e81de9e55880f0c3968f1b
Antonio Viggiano <agfviggiano@gmail.com>
2018-04-26 15:55:38 -0300

    CVC-251 Revert removal of es-lint-ignore

--- test/sessionToken.test.js
+++ test/sessionToken.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 const { expect } = require('chai');
 const session = require('../src/sessionToken');
 const { config } = require('../assets/tests');
@@ -7,39 +9,39 @@ describe('SessionToken Functions', () => {
   it('create a valid session token', () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1m');
-    expect(token).to.not.be.undefined();
+    expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = sessionToken.validate(token);
     expect(origUserId).to.equal(userId);
-    expect(sessionToken.test.verify(token, 60)).to.be.true();
+    expect(sessionToken.test.verify(token, 60)).to.be.true;
   });
 
   it('validate an expired session token', () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1s');
-    expect(token).to.not.be.undefined();
+    expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = sessionToken.validate(token);
     expect(origUserId).to.equal(userId);
-    expect(sessionToken.test.verify(token, 1)).to.be.true();
+    expect(sessionToken.test.verify(token, 1)).to.be.true;
   });
 
   it('validate an session token from event', () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1m');
-    expect(token).to.not.be.undefined();
+    expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const userId = sessionToken.validateFromEvent({ headers: { Authorization: token } });
     expect(origUserId).to.equal(userId);
-    expect(sessionToken.test.verify(token, 60)).to.be.true();
+    expect(sessionToken.test.verify(token, 60)).to.be.true;
   });
 
   it('renew an session token from event', () => {
     const origUserId = 'userid-1';
     const token = sessionToken.create(origUserId, '1m');
-    expect(token).to.not.be.undefined();
+    expect(token).to.not.be.undefined;
     expect(token).to.be.a('string');
     const newToken = sessionToken.keepAliveFromEvent({ headers: { Authorization: token } });
-    expect(sessionToken.test.verify(newToken, 60)).to.be.true();
+    expect(sessionToken.test.verify(newToken, 60)).to.be.true;
   });
 });
